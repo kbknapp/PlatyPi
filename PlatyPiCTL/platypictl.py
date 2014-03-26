be8cc38b204ceca045c72411190075eb71bdb276
kbknapp <kbknapp@gmail.com>
2014-03-26 12:48:27 -0400

    modified:   PlatyPiCTL/platypictl.py

--- PlatyPiCTL/platypictl.py
+++ PlatyPiCTL/platypictl.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/python
+#
+# Python 3.x
+#
+# PlatyPiCtl.py v0.1
+#
+import pifacecad
+from pifacecad.tools.question import LCDQuestion
+
+VERSION_STR = "v0.1"
+
+initial_q = "PlatyPi %s" % VERSION_STR
+options = ["Cmds", "About"]
+
+cad = pifacecad.PiFaceCAD()
+qestion = LCDQuestion(initial_q, options)
+answer_index = question.ask()
