37bab2c0c49d2de6e884cacef0855f4f998bca45
kbknapp <kbknapp@gmail.com>
2014-04-05 21:07:39 -0400

    Changes to be committed: 	modified:   src/libs/pifacecad/pifacecommon/__init__.py

--- src/libs/pifacecad/pifacecommon/__init__.py
+++ src/libs/pifacecad/pifacecommon/__init__.py
@@ -14,5 +14,3 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from .mcp23s17 import mcp23s17
-from .interrupts import interrupts
