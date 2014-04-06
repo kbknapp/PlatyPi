2362b8c8b23b2f6e3c34a20e54947a49a11b9359
kbknapp <kbknapp@gmail.com>
2014-04-05 20:39:22 -0400

    Changes to be committed: 	modified:   src/libs/pifacecad/__init__.py

--- src/libs/pifacecad/__init__.py
+++ src/libs/pifacecad/__init__.py
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from pifacecommon.interrupts import (
+from .pifacecommon.interrupts import (
     IODIR_FALLING_EDGE,
     IODIR_RISING_EDGE,
     IODIR_ON,
