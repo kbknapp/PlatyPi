0d1ba8143882ba255a2d2364aed19143d57f0ff3
kbknapp <kbknapp@gmail.com>
2014-04-11 20:20:24 -0400

    Changes to be committed: 	modified:   platypi/loader.py 	modified:   platypi/platypi.py 	deleted:    platypi/ppmodules/CADCommand.py

--- platypi/platypi.py
+++ platypi/platypi.py
@@ -1,35 +1,104 @@
-#!/usr/bin/python
-#
-# Python 3.x
-#
-# platypi.py
-#
-# v0.1
-#	*
-#
-# Controls the PlatyPi
-#
-import pifacecad
-from time import sleep
-from ppmodules import PPModule
+#!/usr/bin/env python
+"""
+Python 3.x
 
-_TITLE = "PlatyPi v0.1"
+platypi.py
 
-if __name__ == "__main__":
-	# Set up CAD
-	cad = pifacecad.PiFaceCAD()
-	cad.lcd.blink_off()
-	cad.lcd.cursor_off()
-	cad.lcd.backlight_off()
-	cad.lcd.clear()
-	cad.lcd.write(_TITLE)
-	
-	ppm = PPModule(cad, _TITLE)
-	ppm.start()
-	
-	
-	cad.lcd.clear()
-	cad.lcd.home()
-	cad.lcd.write("Exiting...")
-	sleep(3)
-	cad.lcd.display_off()
+Controls the platypi system
+"""
+import sys
+import os
+import itertools
+from threading import Barrier        # Python 3
+
+try:
+    import pifacecad
+except ImportError:
+    print("'pifacecad'' module not installed, using console exclusivly.")
+
+from . import loader
+
+PY3 = sys.version_info[0] >= 3
+if not PY3:
+    print('Requires Python 3.x\n\nExiting...')
+    sys.exit(1)
+
+VERSION = 0.1
+PPMOD_DIR = 'ppmodules'
+ROCKER_RIGHT = 7
+ROCKER_LEFT = 6
+ROCKER_PUSH = 5
+
+__cad = None
+__options = None
+__dirs = []
+__commands = []
+
+
+def main():
+    """Entry point for the platypi system
+    Return: Status (0 or 1)
+    """
+    global __cad
+    __cad = pifacecad.PiFaceCAD()
+    __cad.lcd.blink_off()
+    __cad.lcd.cursor_off()
+    __cad.lcd.write('PlatyPi v{}'.format(VERSION))
+
+    global __dirs
+    global __commands
+    (__dirs, __commands) = loader.find_ppmodules(os.path.join(
+                                            os.path.absdir(__file__),
+                                            PPMOD_DIR
+                                            ))
+    global __options
+    __options = make_options(__dirs, __commands)
+
+    global __exit_barrier
+    __exit_barrier = Barrier(2)
+
+    listener = pifacecad.SwitchEventListener(chip=__cad)
+    listener.register(pifacecad.IODIR_ON, ROCKER_RIGHT, next_option)
+    listener.register(pifacecad.IODIR_ON, ROCKER_LEFT, previous_option)
+    listener.register(pifacecad.IODIR_ON, ROCKER_PUSH, do_option)
+    listener.activate()
+
+    __exit_barrier.wait()
+    close()
+    listener.deactivate()
+
+    return 0
+
+
+def next_option(event=None):
+    update_display(next(__options))
+
+
+def previous_option(event=None):
+    to_advance = (len(__dirs) + len(__commands)) - 2
+    update_display(next(itertools.islice(__options, to_advance, to_advance),
+                        None))
+
+
+def do_option(event=None):
+    pass
+
+
+def make_options(dirs, cmds):
+    return itertools.cycle(itertools.chain(__dirs, __commands))
+
+
+def update_display(line):
+    lcd = __cad.lcd
+    lcd.set_cursor(0, 1)
+    lcd.write(' ' * pifacecad.LCD_WIDTH)
+    lcd.set_cursor(0, 1)
+    lcd.write(line)
+
+
+def close():
+    __cad.lcd.clear()
+    __cad.lcd.disp_off()
+
+if __name__ == '__main__':
+    sys.exit(main())
