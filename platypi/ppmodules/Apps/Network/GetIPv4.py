0da2bd2a8306ce14cf782f29e02e1c3ade76d8d7
Kevin K <kbknapp@gmail.com>
2014-05-08 22:13:57 -0400

    Fixed getipv4 error

--- platypi/ppmodules/Apps/Network/GetIPv4.py
+++ platypi/ppmodules/Apps/Network/GetIPv4.py
@@ -14,7 +14,7 @@ import pifacecad
 MOD_VERSION = '0.1'
 
 def get_ipv4():
-    return subprocess.check_output('ifconfig | grep broadcast | awk "{print $2}"', shell=True)
+    return subprocess.check_output('ifconfig | grep broadcast | awk "{print $2}"', shell=True).decode('utf-8')
 
 def run(cad=None):
     if cad is None:
