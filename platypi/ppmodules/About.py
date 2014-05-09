9cd1e27f0468eb003ab7be711597b58fe3c28716
Kevin K <kbknapp@gmail.com>
2014-05-09 15:15:25 -0400

    Fixed import error

--- platypi/ppmodules/About.py
+++ platypi/ppmodules/About.py
@@ -5,14 +5,12 @@ Displays information about the PlatyPi tool
 
 import pifacecad
 
-from .platypi import VERSION
-
 def run(cad=None):
     if cad is None:
         cad = pifacecad.PiFaceCAD()
         cad.lcd.blink_off()
         cad.lcd.cursor_off()
-    cad.lcd.write('PlatyPi v{}'.format(VERSION))
+    cad.lcd.write('PlatyPi Toolkit')
     cad.lcd.set_cursor(0, 1)
     cad.lcd.write('github.com/kbknapp/PlatyPi')
     cad.lcd.move_right()
